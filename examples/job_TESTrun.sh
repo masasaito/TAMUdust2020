5039d58ba566181505cb7ae903b9becc102838c8
masasaito <61063515+masasaito@users.noreply.github.com>
2020-12-10 16:18:36 -0600

    Update job_TESTrun.sh

--- examples/job_TESTrun.sh
+++ examples/job_TESTrun.sh
@@ -7,9 +7,9 @@
 ../bin/tamudust2020create TAMUdust2020create_exp2.nml
 
 # TAMUdust2020single_exp1 (single-scatteing property of a given dust properties in shortwave 0.2-4 µm)  
-../bin/tamudust2020single TAMUdust2020single_exp1.nml 5.0 0.532 1.5 0.003 0.7 ./output/TAMUdust2020single_exp1
+../bin/tamudust2020single TAMUdust2020single_exp1.nml 5.0 0.532 1.5 0.003 0.7 ./
 
 # TAMUdust2020single_exp2 (single-scatteing property of a given dust properties in shortwave 4-100 µm)  
-../bin/tamudust2020single TAMUdust2020single_exp2.nml 5.0 11.0  1.5 0.3   0.7 ./output/TAMUdust2020single_exp2
+../bin/tamudust2020single TAMUdust2020single_exp2.nml 5.0 11.0  1.5 0.3   0.7 ./
 
 
